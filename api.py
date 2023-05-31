52321db50fbd090a17127cb81fb3b3bcdcaca41c
honey saxena <honeysaxena75@gmail.com>
2023-05-31 19:23:24 +0530

    Third commit

--- api.py
+++ api.py
@@ -1,69 +1,136 @@
 # import requests
-from requests import get, post
+import requests
 import json
 from dotenv import load_dotenv
 import json
 import os
 import base64
+import datetime
+from urllib.parse import urlencode
 
 load_dotenv()
 
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
+class SpotifyAPI(object):
+    access_token = None
+    access_token_expires = datetime.datetime.now()
+    access_token_did_expire = True
+    client_id = None
+    client_secret = None
+    token_url = "https://accounts.spotify.com/api/token"
+    
+    def __init__(self, client_id, client_secret, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.client_id = client_id
+        self.client_secret = client_secret
 
-def get_token():
-    auth_string = client_id + ":" + client_secret
-    auth_bytes = auth_string.encode("utf-8")
-    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
-    url = "https://accounts.spotify.com/api/token"
-    headers = {
-        "Authorization": "Basic " + auth_base64,
-        "Content-Type": "application/x-www-form-urlencoded",
-    }
-    data = {"grant_type": "client_credentials"}
-    result = post(url, headers=headers, data=data)
-    json_result = json.loads(result.content)
-    token = json_result["access_token"]
-    return token
+    def get_client_credentials(self):
+        """
+        Returns a base64 encoded string
+        """
+        client_id = self.client_id
+        client_secret = self.client_secret
+        if client_secret == None or client_id == None:
+            raise Exception("You must set client_id and client_secret")
+        client_creds = f"{client_id}:{client_secret}"
+        client_creds_b64 = base64.b64encode(client_creds.encode())
+        return client_creds_b64.decode()
+    
+    def get_token_headers(self):
+        client_creds_b64 = self.get_client_credentials()
+        return {
+            "Authorization": f"Basic {client_creds_b64}",
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+    
+    def get_token_data(self):
+        return {
+            "grant_type": "client_credentials"
+        }
+    
+    def perform_auth(self):
+        token_url = self.token_url
+        token_data = self.get_token_data()
+        token_headers = self.get_token_headers()
+        r = requests.post(token_url, data=token_data, headers=token_headers)
+        if r.status_code not in range(200, 299):
+            raise Exception("Could not authenticate client.")
+        data = r.json()
+        now = datetime.datetime.now()
+        access_token = data['access_token']
+        expires_in = data['expires_in'] # seconds
+        expires = now + datetime.timedelta(seconds=expires_in)
+        self.access_token = access_token
+        self.access_token_expires = expires
+        self.access_token_did_expire = expires < now
+        return True
+    
+    def get_access_token(self):
+        token = self.access_token
+        expires = self.access_token_expires
+        now = datetime.datetime.now()
+        if expires < now:
+            self.perform_auth()
+            return self.get_access_token()
+        elif token == None:
+            self.perform_auth()
+            return self.get_access_token()
+        return token
+    
+    def get_resource_header(self):
+        access_token = self.get_access_token()
+        headers = {
+            "Authorization": f"Bearer {access_token}"
+        }
+        return headers
+    
+    def get_resource(self, lookup_id, resource_type='albums', version='v1'):
+        endpoint = f"https://api.spotify.com/{version}/{resource_type}/{lookup_id}"
+        headers = self.get_resource_header()
+        r = requests.get(endpoint, headers=headers)
+        if r.status_code not in range(200, 299):
+            return {}
+        return r.json()
+
+    def get_album(self, _id):
+        return self.get_resource(_id, resource_type="albums")
+        
+
+    def get_artist(self, _id):
+        return self.get_resource(_id, resource_type="artists")
+    
+    def base_search(self, query_params):
+        headers = self.get_resource_header()
+        endpoint = "https://api.spotify.com/v1/search"
+        lookup_url = f"{endpoint}?{query_params}"
+        print(lookup_url)
+        r = requests.get(lookup_url, headers=headers)
+        print(r.status_code)
+        if r.status_code not in range(200, 209):
+            return {}
+        return r.json()
+    
+    def search(self, query=None, search_type="artists"):
+        if query == None:
+            raise Exception("A query is required")
+        if isinstance(query, dict):
+            query = " ".join([f"{k}:{v}" for k, v in query.items()])
+        query_params = urlencode({"q": query , "type": search_type.lower()})
+        print(query_params)
+        return self.base_search(query_params)
+
+spotify = SpotifyAPI(client_id, client_secret)    
+print(spotify.perform_auth())
+#print(spotify.search("Time", search_type="Track"))
+#print(spotify.get_artist("0TnOYISbd1XYRBk9myaseg"))
+#access_token = spotify.access_token
+#print(access_token)
+print(spotify.search({"track": "Time"}, search_type="track"))
 
 
-def get_auth_header(token):
-    return {"Authorization": "Bearer " + token}
 
-artist_name = input("Enter artist name: " )
 
-def search_for_artist(token, artist_name):
-    url = "https://api.spotify.com/v1/search"
-    headers = get_auth_header(token)
-    query = f"?q={artist_name}&type=artist&limit=1"
-    query_url = url + query
-    result = get(query_url, headers=headers)
-    json_result = json.loads(result.content)["artists"]["items"]
-    if len(json_result) == 0:
-        print("No artist with this name exists...")
-        return None
-    return json_result[0]
 
-'''
-def get_songs_by_artist(token, artist_id):
-    url = "https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
-    headers = get_auth_header(token)
-    result = get(url, headers=headers)
-    json_result = json.loads(result.content)
-    return json_result
-'''
 
-token = get_token()
-print(token)
-
-result = search_for_artist(token, artist_name)
-print(result)
-artist_id = result["id"]
-print(artist_id)
-#songs = get_songs_by_artist(token, artist_id)
-#print(songs)
-"""
-for idx, song in enumerate(songs):
-    print(f"{idx + 1}. {song['name']}")
-"""
---
+++ playlist.py
@@ -0,0 +1,19 @@
+from urllib.parse import urlencode
+import api
+import requests
+
+
+
+headers = {
+    "Authorization": f"Bearer {api.spotify.access_token}"
+}
+endpoint = "https://api.spotify.com/v1/search"
+data = urlencode({"q": "Time", "type": "track"})
+print(data)
+
+lookup_url = f"{endpoint}?{data}"
+print(lookup_url)
+
+r = requests.get(lookup_url, headers=headers)
+print(r.status_code)
+print(r.json())
