bae0b46c7628b0d5176f9a0ea38f3bbbd219109b
Honey saxena <honeysaxena525@gmail.com>
2023-06-08 01:49:44 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,22 +1,46 @@
 import pathlib
-from cassandra.cluster import Cluster
-from cassandra.auth import PlainTextAuthProvider
-from cassandra.cqlengine import connection
+#from cassandra.cluster import Cluster
+#from cassandra.auth import PlainTextAuthProvider
+#from cassandra.cqlengine import connection
+from application import config
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 BASE_DIR = pathlib.Path(__file__).resolve().parent
 print(BASE_DIR)
 
-ASTRADB_CONNECT_BUNDLE = BASE_DIR / "connect_bundle" / "astradb_connect.zip"
-ASTRADB_CLIENT_ID = '<<CLIENT ID>>'
-ASTRADB_CLIENT_SECRET = '<<CLIENT SECRET>>'
+settings = config.get_settings()
 
-def get_session():
-    cloud_config= {
-    'secure_connect_bundle': ASTRADB_CONNECT_BUNDLE
-    }
-    auth_provider = PlainTextAuthProvider(ASTRADB_CLIENT_ID, ASTRADB_CLIENT_SECRET)
-    cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
-    session = cluster.connect()
-    connection.register_connection(str(session), session=session)
-    connection.set_default_connection(str(session))
-    return session
+
+
+#ASTRADB_CONNECT_BUNDLE = BASE_DIR / "connect_bundle" / "astradb_connect.zip"
+#ASTRADB_CLIENT_ID = settings.db_client_id
+#ASTRADB_CLIENT_SECRET = settings.db_client_secret
+
+postgres_client_password = settings.db_client_password
+
+
+
+DATABASE_URL = "postgresql://postgres:postgres_client_password@localhost:5432/api"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+#def get_session():
+#    DATABASE_URL = "postgresql://POSTGRES_CLIENT_USERNAME:POSTGRES_CLIENT_PASSWORD@localhost:5432/api"
+#    engine = create_engine(DATABASE_URL)
+#    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    
+#    return SessionLocal
+
+def get_db():
+    dbs = SessionLocal()
+    try:
+        yield dbs
+    finally:
+        dbs.close()    
+
+
+
